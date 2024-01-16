a37b2a2f265ec72d8ceaf374170ca007e26457fb
Hussain101 <hussainsidd88@gmail.com>
2024-01-16 23:29:33 +0500

    question mark

--- auth.ts
+++ auth.ts
@@ -26,10 +26,10 @@ export const { auth, signIn, signOut } = NextAuth({
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
           if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
+            const { email, password } = parsedCredentials?.data;
             const user = await getUser(email);
             if (!user) return null;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
+            const passwordsMatch = await bcrypt.compare(password, user?.password);
  
             if (passwordsMatch) return user;
           }
